62e6a036dfa3fc5b7a702a40f5e4d7ffb0d4602a
Vinny <92878154+VinnyBonner@users.noreply.github.com>
2022-12-12 13:25:25 -0800

    Create Function1.cs

---
+++ Function1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging;
+using Azure.Messaging.EventGrid;
+using Azure.Messaging.EventHubs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FunctionApp13
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static void Run([EventGridTrigger] EventGridEvent eventGridEvent,
+        [EventHub("myeventhub", Connection = "EventHubConnectionAppSetting")] out string outputEvents,
+        ILogger log)
+        {
+            try
+            {
+                log.LogInformation("triggering");
+                log.LogInformation(eventGridEvent.Data.ToString());
+                outputEvents = eventGridEvent.Data.ToString();
+            }
+            catch (Exception ex)
+            {
+                log.LogError("ERROR: " + ex.ToString());
+                outputEvents = "Failed";
+            }
+        }
+    }
+}
